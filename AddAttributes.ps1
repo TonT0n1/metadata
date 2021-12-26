b29c533e99b85d5bc9c33494c5dd8ff7067cb419
TonT0n1 <antonyk86@gmail.com>
2021-12-26 13:32:17 +0000

    AddAttributes

---
+++ AddAttributes.ps1
@@ -0,0 +1,39 @@
+﻿########################## Define your values here ###########################
+
+$pathToJson = 'C:\temp\json-2' <# Enter path to existing JSON files to update/modify #>
+$pathToList = 'C:\temp\duties.txt' <# Enter path to text file that contains list of values listed on individual lines #>
+$outputPath = 'c:\temp\new-JSON' <# Enter desired output path - NO TRAILING \ at the end #>
+$traitType = 'Duty'<# Enter trait type name here #>
+
+if (!(Test-Path -Path $outputPath)) {
+
+New-Item -Path $outputPath -ItemType Directory
+
+}
+
+##############################################################################
+
+###################### Get Item list and JSON Files ##########################
+
+$values = Get-Content $pathToList
+$files = Get-ChildItem -Path $pathToJson
+
+##############################################################################
+
+## Loop through each file and dynamically add new value at random from list ##
+
+foreach ($file in $files){
+
+$data = $file.FullName
+$fileNm = $file.Name
+$assigned = Get-Random -InputObject $values
+
+$content = get-content $data
+
+$json = $content | ConvertFrom-Json
+
+$json.attributes += New-Object -TypeName psobject -Property @{value="$assigned"; trait_type="$traitType"}
+
+$json | ConvertTo-Json | out-file $outputPath\$fileNm
+
+}
